c0c5577cb76ddf7db03645f52c4ae172c4bc5acb
Арина Головина <144282698+AMGolovina@users.noreply.github.com>
2026-02-08 22:03:23 +0300

    Create HashFuncGen.h

---
+++ SET5/A3/HashFuncGen.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+
+class HashFuncGen {
+public:
+    explicit HashFuncGen(uint32_t seed = 0) : seed_(seed) {}
+
+    uint32_t operator()(const std::string& s) const {
+        uint32_t h = 2166136261u ^ seed_;
+        for (unsigned char c : s) {
+            h ^= static_cast<uint32_t>(c);
+            h *= 16777619u;
+        }
+        return h;
+    }
+
+private:
+    uint32_t seed_;
+};
